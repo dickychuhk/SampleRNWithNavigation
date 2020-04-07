b367f17a21686ec14dd9230c40ce8754f2f561f8
Dicky <>
2020-04-07 20:59:05 +0800

    - Change Signup Page to use Class Component

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-const SignupScreen = ()=>{
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Welcome to Signup screen</Text>
-        </View>
-    );
-};
+// Funtional Component
+// const SignupScreen = ()=>{
+//     return (
+//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//             <Text>Welcome to Signup screen</Text>
+//         </View>
+//     );
+// };
+
+//Class Component
+class SignupScreen extends Component {
+
+    render(){
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Welcome to Signup screen</Text>
+            </View>
+        );
+    }
+}
+
 
 
 export default SignupScreen;
